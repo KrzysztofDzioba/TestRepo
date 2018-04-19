e60a05db4f8b177ffc4ded4d6ad84f4f78e4932b
KrzysztofDzioba <krzysztof.dzioba.93@gmail.com>
2018-04-19 17:58:07 +0200

    commit3

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    public void test1(){
+        assertEquals("1", "2");
+    }
+
+}
