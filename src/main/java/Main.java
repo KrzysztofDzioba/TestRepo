dae515bf4737ad507587fa7189bfbcccde0d378a
Krzysztof Dzioba <krzysztof_dzioba@epam.com>
2018-05-18 16:30:31 +0200

    Ten commit bedzie zrobiony

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,7 @@ public class Main {
         System.out.println(Objects.equals(kl, kll));
         MyInterface.createAction();
         kl.czesc();
-
+        System.out.println("Czeesc");
 
     }
 
